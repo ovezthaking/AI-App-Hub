04ca801a76b8b120a41b4e3ea8b6c00ed4d7053d
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-30 16:25:35 +0100

    fix: update document splitting parameters and enhance embedding retrieval process

--- LangChain/index.js
+++ LangChain/index.js
@@ -8,8 +8,8 @@ async function splitDocument(document) {
     const text = await response.text()
 
     const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 150,
-        chunkOverlap: 15,
+        chunkSize: 250,
+        chunkOverlap: 35,
     })
 
     const output = await splitter.createDocuments([text])
@@ -20,7 +20,37 @@ async function splitDocument(document) {
 
 async function createAndStoreEmbeddings() {
     const chunkData = await splitDocument('movies.txt')
-    console.log(chunkData)
+    
+    const data = await Promise.all(
+        chunkData.map(async (chunk) => {
+            const embedding = await getEmbeddings(chunk.pageContent)
+            
+            return {
+                content: chunk.pageContent,
+                embedding: embedding.embeddings
+            }
+        })
+    )
+
+    // await supabase.from('movies').insert(data)
+    console.log('success')
+}
+
+
+async function getEmbeddings(text, model='sentence-transformers/paraphrase-multilingual-mpnet-base-v2') {
+  const response = await fetch('http://localhost:3000/api/embeddings', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text, model }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return await response.json();
 }
 
 
