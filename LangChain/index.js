3b9f2e2e85b5ab9b3a98922fe0449e001d4d4301
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-30 16:28:59 +0100

    fix: improve error handling in document splitting function

--- LangChain/index.js
+++ LangChain/index.js
@@ -4,36 +4,54 @@ import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 const document = await fetch('./movies.txt').then(res => res.text())
 
 async function splitDocument(document) {
-    const response = await fetch(`./${document}`)
-    const text = await response.text()
+    try {
+        const response = await fetch(`./${document}`)
 
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 250,
-        chunkOverlap: 35,
-    })
+        if (!response.ok){
+            throw new Error('Network response was not ok.')
+        }
 
-    const output = await splitter.createDocuments([text])
-
-    return output
+        const text = await response.text()
+    
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 250,
+            chunkOverlap: 35,
+        })
+    
+        const output = await splitter.createDocuments([text])
+    
+        return output
+    } catch (e) {
+        console.error('There was an issue with splitting text ')
+        throw e
+    }
 }
 
 
 async function createAndStoreEmbeddings() {
-    const chunkData = await splitDocument('movies.txt')
+    try {
+        const chunkData = await splitDocument('movies.txt')
+        
+        const data = await Promise.all(
+            chunkData.map(async (chunk) => {
+                const embedding = await getEmbeddings(chunk.pageContent)
+                
+                return {
+                    content: chunk.pageContent,
+                    embedding: embedding.embeddings
+                }
+            })
+        )
     
-    const data = await Promise.all(
-        chunkData.map(async (chunk) => {
-            const embedding = await getEmbeddings(chunk.pageContent)
-            
-            return {
-                content: chunk.pageContent,
-                embedding: embedding.embeddings
-            }
-        })
-    )
-
-    // await supabase.from('movies').insert(data)
-    console.log('success')
+        // const { error } = await supabase.from('movies').insert(data)
+        // if (error) {
+        //     throw new Error('Issue inserting data into the database.');
+        // }
+        
+        console.log('success')
+    } catch (e) {
+        console.error('Error: ', e.message)
+    }
 }
 
 
