79ef9af0feacc179cba1a9fac0f94b6c4155c97d
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-31 03:30:42 +0100

    feat: implement embedding creation and storage functions for movie data

--- PopChoice/index.js
+++ PopChoice/index.js
@@ -1,1 +1,40 @@
 import { hf, supabase } from "./config";
+import content from "./content";
+
+const createEmbedding = async (input) => {
+    const embeddingResponse = await hf.featureExtraction({
+        model: 'Qwen/Qwen3-Embedding-8B',
+        inputs: input
+    })
+
+    return embeddingResponse
+}
+
+const storeJsEmbeddings = async (input) => {
+    try {
+        const data = await Promise.all(
+            input.map( async (textChunk) => {
+                const text = `${textChunk.title}, ${textChunk.releaseYear}: ${textChunk.content}`
+    
+                const embeddingResponse = createEmbedding(text)
+    
+                return {
+                    content: text,
+                    embedding: embeddingResponse
+                }
+            })
+        )
+
+        if (!data) {
+            throw new Error('No data in array')
+        }
+        
+        console.log(data)
+        await supabase.from('popchoice').insert(data)
+        console.log('Embeddings inserted!')
+    } catch (err) {
+        console.error('Error: ', err)
+    }
+}
+
+// storeJsEmbeddings(content)
