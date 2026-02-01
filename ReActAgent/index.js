2d5cbb09fd9e30ac71cfb924d2230c735b66f4c8
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-01 01:14:51 +0100

    feat: implement weather and location retrieval functions for activity suggestions

--- ReActAgent/index.js
+++ ReActAgent/index.js
@@ -1,14 +1,20 @@
 import { InferenceClient } from "@huggingface/inference";
+import { getCurrentWeather, getLocation } from "./tools";
 
 
 const hf = new InferenceClient(import.meta.env.VITE_HF_TOKEN)
+
+// Call a function to get current location and current weather
+
+const weather = await getCurrentWeather()
+const location = await getLocation()
 
 const response = await hf.chatCompletion({
     model: 'Qwen/Qwen3-235B-A22B-Instruct-2507',
     messages: [
         {
             role: 'user',
-            content: 'Give me a list of activity ideas based on my current location and weather'
+            content: `Give me a list of activity ideas based on my current location of ${location} and weather of ${weather}`
         }
     ]
 })
