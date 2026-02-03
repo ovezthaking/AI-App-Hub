e8a3d55b88e7263fe89267a3c5c2dfb7ab4ef674
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-03 21:06:09 +0100

    Cheaper but a little less smart model. But faster at least

--- FunctionsAgent/index.js
+++ FunctionsAgent/index.js
@@ -19,7 +19,7 @@ conversational responses, but only include the normal information a
 regular person might want unless they explicitly ask for more. Provide 
 highly specific answers based on the information you're given. Prefer 
 to gather information with the tools provided to you rather than 
-giving basic, generic answers.
+giving basic, generic answers. Don't give answers in tags. Just text.
 `
     },
 ]
@@ -37,7 +37,7 @@ async function agent(query) {
     for (let i = 0; i < MAX_ITERATIONS; i++) {
         // console.log(`Iteration #${i+1}`)
         const response = await hf.chatCompletion({
-            model: 'Qwen/Qwen3-235B-A22B-Instruct-2507',
+            model: 'XiaomiMiMo/MiMo-V2-Flash',
             messages,
             tools
         })
